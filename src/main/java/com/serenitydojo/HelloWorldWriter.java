9077568a0529d2521ff21c273c90bf7d2cbd305c
sivareddy1 <siva.reddy2211@gmail.com>
2021-05-26 17:06:19 +0100

    First commit by Siva

---
+++ src/main/java/com/serenitydojo/HelloWorldWriter.java
@@ -0,0 +1,11 @@
+package com.serenitydojo;
+
+//written by Siva
+public class HelloWorldWriter {
+
+    public void writeHelloWorld(){
+
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/test/java/com/serenitydojo/HelloWorldWriterTest.java
@@ -0,0 +1,15 @@
+package com.serenitydojo;
+
+import org.junit.Test;
+
+//written by Siva
+public class HelloWorldWriterTest {
+
+    @Test
+    public void shouldWriteHelloWorldToConsole(){
+
+        HelloWorldWriter writer = new HelloWorldWriter();
+        writer.writeHelloWorld();
+
+    }
+}
