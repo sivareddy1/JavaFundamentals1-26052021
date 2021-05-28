a489c8530bec23452d28f6e13a2659316af075b6
sivareddy1 <siva.reddy2211@gmail.com>
2021-05-28 07:56:23 +0100

    Up to javaClassesAndObjects

---
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -0,0 +1,36 @@
+package com.serenitydojo;
+
+import org.junit.Test;
+
+public class WhenCreatingObjects {
+
+    @Test
+    public void creating_a_cat(){
+        String name = "Felix";
+        String favoriteFood = "Tuna";
+        int age = 4;
+
+        /*
+        Cat felix = new Cat();
+        //felix.name = "Felix";
+        felix.setName("Felix");
+        System.out.println("Cat name is " + felix.getName());
+        //felix.favoriteFood = "Tuna";
+        felix.setFavoriteFood("Tuna");
+        System.out.println(felix.getName() + " favorite food is " + felix.getFavoriteFood());
+        felix.age = 5;
+
+        Cat fino = new Cat();
+        fino.name = "Fino";
+        fino.age = 3;
+        */
+
+        Cat felix = new Cat("Felix", "Tuna",5);
+        System.out.println("Cat name is " + felix.getName());
+        System.out.println(felix.getName() + " favorite food is " + felix.getFavoriteFood());
+        System.out.println(felix.getName() + " age is " + felix.getAge());
+
+        //felix.age = 10;
+
+    }
+}
